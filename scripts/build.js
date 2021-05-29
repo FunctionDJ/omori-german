eb73d79f7c63043ce2595f9d2b1afa1d08dd3297
Function <34871211+FunctionDJ@users.noreply.github.com>
2021-05-29 18:10:17 +0200

    init

---
+++ scripts/build.js
@@ -0,0 +1,16 @@
+import { promises as fs } from "fs"
+import zip from "bestzip"
+
+;(async () => {
+  const mod = await fs.readFile("./mod.json").then(JSON.parse)
+
+  const includes = Object.values(mod.files).flat()
+
+  zip({
+    source: [
+      "mod.json",
+      ...includes
+    ],
+    destination: "./omori_de.zip"
+  })
+})()
